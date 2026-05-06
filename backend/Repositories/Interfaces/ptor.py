a695535298d8042c868a042f3ac9e80e86256150
DorBilia <dor.bilia2@gmail.com>
2026-05-06 19:37:10 +0300

    added ptor interfaces

--- backend/Repositories/Interfaces/ptor.py
+++ backend/Repositories/Interfaces/ptor.py
@@ -1,5 +1,5 @@
 from base_repo import IBaseRepo
-from db.models.ptor import MedicalPtor, MedicalPtorType, BeardStatementType, BeardStatement
+from db.models.ptor import MedicalPtor, BeardStatement
 from typing import Sequence
 from abc import abstractmethod
 
@@ -7,6 +7,13 @@ from abc import abstractmethod
 class IMedicalPtorRepo(IBaseRepo[MedicalPtor]):
 
     @abstractmethod
-    async def get_for_soldier(self, soldier_id: int) -> Sequence[MedicalPtor]:
+    async def get_by_ptor_type(self, ptor_type: int) -> Sequence[MedicalPtor]:
+        # Return a complete ptor + soldier information
         pass
 
+
+class IBeardStatementRepo(IBaseRepo[BeardStatement]):
+
+    @abstractmethod
+    async def get_by_statement_type(self, statement_type: int) -> Sequence[BeardStatement]:
+        pass
