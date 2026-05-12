9ca5781fe27e06640295e6356df692da96998956
DorBilia <dor.bilia2@gmail.com>
2026-05-12 13:04:38 +0300

    implemented soldier repo

---
+++ backend/Repositories/Interfaces/soldier.py
@@ -0,0 +1,35 @@
+from baseRepo import IBaseRepo
+from db.models.soldier import Soldier, Doh1
+from typing import Sequence
+from abc import abstractmethod
+
+
+class ISoldierRepo(IBaseRepo[Soldier]):
+
+    @abstractmethod
+    async def get_by_unit(self, unit_id: str) -> Sequence[Soldier]:
+        pass
+
+    @abstractmethod
+    async def get_by_branch(self, branch: str) -> Sequence[Soldier]:
+        pass
+
+    @abstractmethod
+    async def get_by_department(self, branch: str) -> Sequence[Soldier]:
+        pass
+
+    @abstractmethod
+    async def get_by_rank(self, rank: str) -> Sequence[Soldier]:
+        pass
+
+    @abstractmethod
+    async def get_by_discharge_date(self, discharge_date: str) -> Sequence[Soldier]:
+        pass
+
+    @abstractmethod
+    async def get_by_service_type(self, service_type: str) -> Sequence[Soldier]:
+        pass
+
+    @abstractmethod
+    async def get_by_phone_number(self, phone_number: str) -> Sequence[Soldier]:
+        pass
--- backend/Repositories/soldier.py
+++ backend/Repositories/soldier.py
@@ -1,39 +1,47 @@
-# from sqlalchemy import select, update, delete
-# from typing import Optional, Sequence
-# from db.models.Soldier import Soldier
-# from Repositories import abstract_repo
-# from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, update, delete
+from typing import Optional, Sequence
+from db.models.soldier import Soldier
+from Repositories.AbstractRepo import AbstractRepo
+from Repositories.Interfaces.soldier import ISoldierRepo
+from sqlalchemy.ext.asyncio import AsyncSession
 
-#
-# class SoldierRepository(AbstractRepo):
-#
-#     async def create(self, **kwargs) -> Soldier:
-#         new_soldier = Soldier(
-#             **kwargs
-#         )
-#         self.db.add(new_soldier)
-#         await self.db.commit()
-#         await self.db.refresh(new_soldier)
-#         return new_soldier
-#
-#     async def get_by_id(self, soldier_id: int) -> Optional[Soldier]:
-#         result = await self.db.execute(select(Soldier).where(Soldier.id == soldier_id))
-#         return result.scalar_one_or_none()
-#
-#     async def update(self, soldier_id: int, **updates) -> Optional[Soldier]:
-#         query = (
-#             update(Soldier)
-#             .where(Soldier.id == soldier_id)
-#             .values(**updates)
-#             .execution_options(synchronize_session="fetch")
-#         )
-#         await self.db.execute(query)
-#         await self.db.commit()
-#         return await self.get_by_id(soldier_id)
-#
-#     async def delete(self, soldier_id: int) -> bool:
-#         query = delete(Soldier).where(Soldier.id == soldier_id)
-#         result = await self.db.execute(query)
-#         await self.db.commit()
-#         return result.rowcount > 0
-#
+
+class SoldierRepository(AbstractRepo[Soldier], ISoldierRepo[Soldier]):
+
+    def __init__(self, db: AsyncSession):
+        super().__init__(db, Soldier)
+
+    async def get_by_unit(self, unit_id: str) -> Sequence[Soldier]:
+        stmt = select(Soldier).where(Soldier.unit == unit_id)
+        result = await self.db.execute(stmt)
+        return result.scalars().all()
+
+    async def get_by_branch(self, branch: str) -> Sequence[Soldier]:
+        stmt = select(Soldier).where(Soldier.branch == branch)
+        result = await self.db.execute(stmt)
+        return result.scalars().all()
+
+    async def get_by_department(self, department: str) -> Sequence[Soldier]:
+        stmt = select(Soldier).where(Soldier.department == department)
+        result = await self.db.execute(stmt)
+        return result.scalars().all()
+
+    async def get_by_rank(self, rank: str) -> Sequence[Soldier]:
+        stmt = select(Soldier).where(Soldier.rank == rank)
+        result = await self.db.execute(stmt)
+        return result.scalars().all()
+
+    async def get_by_discharge_date(self, discharge_date: str) -> Sequence[Soldier]:
+        stmt = select(Soldier).where(Soldier.discharge_date == discharge_date)
+        result = await self.db.execute(stmt)
+        return result.scalars().all()
+
+    async def get_by_service_type(self, service_type: str) -> Sequence[Soldier]:
+        stmt = select(Soldier).where(Soldier.service_type == service_type)
+        result = await self.db.execute(stmt)
+        return result.scalars().all()
+
+    async def get_by_phone_number(self, phone_number: str) -> Sequence[Soldier]:
+        stmt = select(Soldier).where(Soldier.phone_number == phone_number)
+        result = await self.db.execute(stmt)
+        return result.scalars().all()
