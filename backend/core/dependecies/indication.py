6c0a0aa5efc79fa5a6a7610e265c0eb2005b93ad
DorBilia <dor.bilia2@gmail.com>
2026-05-17 14:34:36 +0300

    added relevant dependencies

--- backend/core/dependecies/indication.py
+++ backend/core/dependecies/indication.py
@@ -1,23 +1,40 @@
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from Repositories.indication import IndicationRepository, IndicationTypeRepository
-from Repositories.Interfaces.indication import IIndicationRepo
-from Services.indication import IndicationService
-from Services.Interfaces.indication import IIndicationService
+from Repositories.indication import SoldierIndicationRepository, IndicationTypeRepository, \
+    OrganizationIndicationRepository
+from Repositories.Interfaces.indication import ISoldierIndicationRepo
+from Services.indication import SoldierIndicationService, OrganizationIndicationService, IndicationService
+from Services.Interfaces.indication import ISoldierIndicationService, IOrganizationIndicationService
 
 from db.db import get_db
 
 
-async def get_indication_repository(db: AsyncSession = Depends(get_db)) -> IIndicationRepo:
-    return IndicationRepository(db)
+async def get_soldier_indication_repository(db: AsyncSession = Depends(get_db)) -> ISoldierIndicationRepo:
+    return SoldierIndicationRepository(db)
 
 
 async def get_indication_type_repository(db: AsyncSession = Depends(get_db)) -> IndicationTypeRepository:
     return IndicationTypeRepository(db)
 
 
+async def get_organization_indication_repository(
+        db: AsyncSession = Depends(get_db)) -> OrganizationIndicationRepository:
+    return OrganizationIndicationRepository(db)
+
+
 async def get_indication_service(
-    repository: IIndicationRepo = Depends(get_indication_repository),
-) -> IIndicationService:
-    return IndicationService(repository)
+        type_repository: IndicationTypeRepository = Depends(
+            get_indication_type_repository)) -> IndicationService:
+    return IndicationService(type_repository)
+
+
+async def get_soldier_indication_service(
+        repository: ISoldierIndicationRepo = Depends(
+            get_soldier_indication_repository), ) -> ISoldierIndicationService:
+    return SoldierIndicationService(repository)
+
+
+async def get_organization_indication_service(
+        repository: OrganizationIndicationRepository = Depends(get_organization_indication_repository)):
+    return OrganizationIndicationService(repository)
