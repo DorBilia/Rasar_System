d4599e501eecbfb775e8385d250781657203ac8f
DorBilia <dor.bilia2@gmail.com>
2026-05-17 14:35:08 +0300

    added indication routers, more to add

---
+++ backend/API/routers/indication.py
@@ -0,0 +1,58 @@
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi_restful.cbv import cbv
+from Services.Interfaces.indication import *
+from core.dependecies.indication import get_soldier_indication_service, get_organization_indication_service, \
+    get_indication_service
+from API.schemas.indication import *
+
+indication_router = APIRouter(prefix="/indications", tags=["Indications"])
+soldier_router = APIRouter(prefix="/indications/soldier", tags=["IndicationsSoldier"])
+organization_router = APIRouter(prefix="/indications/organization", tags=["IndicationsOrganisation"])
+
+
+@cbv(indication_router)
+class IndicationRouter:
+    service: IIndicationService = Depends(get_indication_service)
+
+    @indication_router.get("/types", response_model=List[IndicationType])
+    async def get_types(self):
+        return await self.service.get_types()
+
+
+@cbv(soldier_router)
+class IndicationSoldierRouter:
+    service: ISoldierIndicationService = Depends(get_soldier_indication_service)
+
+    @soldier_router.post("/create", response_model=SoldierIndicationResponse)
+    async def create(self, request: SoldierIndicationRequest):
+        result = await self.service.create(request)
+        if result is None:
+            raise HTTPException(status_code=500, detail="somthing went wrong")
+        return result
+
+    @organization_router.get("/{id}", response_model=SoldierIndicationResponse)
+    async def get_by_id(self, id: int):
+        return await self.service.get_by_id(id)
+
+
+@cbv(organization_router)
+class OrganizationSoldierRouter:
+    service: IOrganizationIndicationService = Depends(get_organization_indication_service)
+
+    @organization_router.post("/create", response_model=OrganizationIndicationRequest)
+    async def create(self, request: OrganizationIndicationRequest):
+        result = await self.service.create(request)
+        if result is None:
+            raise HTTPException(status_code=500, detail="somthing went wrong")
+        return result
+
+    @organization_router.get("/", response_model=List[OrganizationIndicationMinimal])
+    async def get_all(self):
+        return await self.service.get_all()
+
+    @organization_router.get("/{id}", response_model=OrganizationIndicationResponse)
+    async def get_by_id(self, id: int):
+        return await self.service.get_by_id(id)
+
+# get-filtered
+
