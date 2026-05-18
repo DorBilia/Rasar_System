a617cc73a4ec5e76539224914dbf62254a90e793
DorBilia <dor.bilia2@gmail.com>
2026-05-18 18:36:54 +0300

    add routers to main

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 import uvicorn
 from settings import settings
 from db.db import create_tables
-from API.routers import soldier
+from API.routers import soldier, indication
 
 app = FastAPI(
     title="Rasar System API",
@@ -15,7 +15,11 @@ app = FastAPI(
 
 async def main():
     await create_tables()
+
     app.include_router(soldier.router, prefix=settings.API_PREFIX)
+    app.include_router(indication.indication_router, prefix=settings.API_PREFIX)
+    app.include_router(indication.soldier_router, prefix=settings.API_PREFIX)
+    app.include_router(indication.organization_router, prefix=settings.API_PREFIX)
 
     config = uvicorn.Config(app, host="localhost", port=8000, reload=True)
 
