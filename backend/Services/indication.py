040e85ed2e6970ef04f525f7be74c2a9eaf2a073
DorBilia <dor.bilia2@gmail.com>
2026-05-20 10:32:24 +0300

    renames

--- backend/Services/indication.py
+++ backend/Services/indication.py
@@ -28,6 +28,11 @@ class SoldierIndicationService(ISoldierIndicationService):
         created = await self._repository.create(**data)
         return SoldierIndicationResponse.model_validate(created)
 
+    # TODO: change return type to a schema
+    async def get_by_indication_type(self, indication_type: IndicationType) -> Sequence[SoldierIndicationResponse]:
+        indications_result = await self._repository.get_by_indication_type(indication_type)
+        return [SoldierIndicationResponse.model_validate(r) for r in indications_result]
+
     async def can_soldier_attend_misdar(self, soldier_id: int, misdar_id: int) -> bool:
         return await self._repository.can_soldier_attend_misdar(soldier_id, misdar_id)
 
