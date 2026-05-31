e48ee19e6e87d92b194d90fa178846eabc0005f5
DorBilia <dor.bilia2@gmail.com>
2026-05-31 21:23:00 +0300

    updated repo to match model

--- backend/Repositories/Interfaces/biror.py
+++ backend/Repositories/Interfaces/biror.py
@@ -6,5 +6,5 @@ from abc import abstractmethod
 
 class IBirorRepo(IBaseRepo[Biror]):
     @abstractmethod
-    async def get_by_result(self, result: str) -> Sequence[Biror]:
+    async def get_by_result(self, result: int) -> Sequence[Biror]:
         pass
